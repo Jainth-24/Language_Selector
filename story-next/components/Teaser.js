8ef478c8d8cd77c133b5941b907df0b7c25febcb
Jainth-24 <136298206+Jainth-24@users.noreply.github.com>
2023-06-14 18:39:53 +0530

    Delete story-next directory

--- story-next/components/Teaser.js
+++
@@ -1,7 +0,0 @@
-import { storyblokEditable } from "@storyblok/react";
-
-const Teaser = ({ blok }) => {
-  return <h2 className="text-2xl mb-10" {...storyblokEditable(blok)}>{blok.header}</h2>;
-};
-
-export default Teaser;
